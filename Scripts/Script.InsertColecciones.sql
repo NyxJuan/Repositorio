c9a84e0517d481540528e62c177deda54c690639
Diego Rolando Layme Valeriano <dvansl@hotmail.com>
2018-08-25 13:44:01 -0500

    Subiendo 3 Tablas  ARticulos , coleccion y Editorial

---
+++ Scripts/Script.InsertColecciones.sql
@@ -0,0 +1,24 @@
+--REGISTROS DE LA TABLA COLECCIONES--
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (1, 'Administración','Administración', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (1, 'Ciencias contables','Ciencias contables', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (1, 'Derecho','Derecho', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (1, 'Derecho penal y política criminal','Derecho penal y política criminal', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (1, 'Educación','Educación', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (2, 'Ciencias de la Salud','Ciencias de la Salud', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (2, 'Ciencias Empresariales','Ciencias Empresariales', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (2, 'Derecho','Derecho', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (2, 'Educación e Investigación','Educación e Investigación', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (2, 'Ingeniería y Arquitectura','Ingeniería y Arquitectura', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (3, 'Escuela Profesional de Arquitectura','Escuela Profesional de Arquitectura del Paisaje', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (3, 'Escuela Profesional de Arquitectura del Paisaje','Escuela Profesional de Arquitectura del Paisaje', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (3, 'Escuela Profesional de Diseño Gráfico','Escuela Profesional de Diseño Gráfico', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (4, 'Escuela Profesional de Medicina Humana','Escuela Profesional de Medicina Humana', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (4, 'Escuela Profesional de Odontología','Escuela Profesional de Odontología', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (4, 'Escuela Profesional de Tecnología Médica','Escuela Profesional de Tecnología Médica', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Administración de Negocios Internacionales','Escuela Profesional de Administración de Negocios Internacionales', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Administración Turístico - Hotelera','Escuela Profesional de Administración Turístico - Hotelera', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Economía y Microfinanzas','Escuela Profesional de Economía y Microfinanzas', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Ingeniería Comercial','Escuela Profesional de Ingeniería Comercial', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Ingeniería de la Producción y Administración','Escuela Profesional de Ingeniería de la Producción y Administración', 1,0,'2018-05-22');
+INSERT INTO tbl_Colecciones (Id_Subcomunidad, Nombre,Descripcion, Estado,Eliminado,Fecha_Registro)VALUES (5, 'Escuela Profesional de Ciencias Contables y Financieras','Escuela Profesional de Ciencias Contables y Financieras', 1,0,'2018-05-22');
+
---
+++ Scripts/Script.InsertEditorial.sql
@@ -0,0 +1,1 @@
+INSERT INTO tbl_Editorial VALUES('Universidad Privada de Tacna','Av. Jorge Basadre Grohmann s/n Pocollay',1,0,'2010-05-10');
