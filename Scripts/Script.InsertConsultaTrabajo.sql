450d03f2371b819ee3d8e8acaad5b52bca53684f
P-TaquilaCc <percy-taquila23@hotmail.com>
2018-08-25 13:21:51 -0500

    Subiendo Querys

---
+++ Scripts/Script.InsertConsultaTrabajo.sql
@@ -0,0 +1,43 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--REGISTROS DE LA TABLA CONSULTA USUARIO--
+
+INSERT INTO tbl_Consulta_Trabajo VALUES(1);
+INSERT INTO tbl_Consulta_Trabajo VALUES(20);
+INSERT INTO tbl_Consulta_Trabajo VALUES(5);
+INSERT INTO tbl_Consulta_Trabajo VALUES(10);
+INSERT INTO tbl_Consulta_Trabajo VALUES(11);
+INSERT INTO tbl_Consulta_Trabajo VALUES(12);
+INSERT INTO tbl_Consulta_Trabajo VALUES(12);
+INSERT INTO tbl_Consulta_Trabajo VALUES(1);
+INSERT INTO tbl_Consulta_Trabajo VALUES(1);
+INSERT INTO tbl_Consulta_Trabajo VALUES(1);
+INSERT INTO tbl_Consulta_Trabajo VALUES(3);
+INSERT INTO tbl_Consulta_Trabajo VALUES(3);
+INSERT INTO tbl_Consulta_Trabajo VALUES(3);
+INSERT INTO tbl_Consulta_Trabajo VALUES(15);
+INSERT INTO tbl_Consulta_Trabajo VALUES(25);
+INSERT INTO tbl_Consulta_Trabajo VALUES(14);
+INSERT INTO tbl_Consulta_Trabajo VALUES(14);
+INSERT INTO tbl_Consulta_Trabajo VALUES(8);
+INSERT INTO tbl_Consulta_Trabajo VALUES(18);
+INSERT INTO tbl_Consulta_Trabajo VALUES(18);
+INSERT INTO tbl_Consulta_Trabajo VALUES(18);
+INSERT INTO tbl_Consulta_Trabajo VALUES(20);
+INSERT INTO tbl_Consulta_Trabajo VALUES(25);
+INSERT INTO tbl_Consulta_Trabajo VALUES(25);
+INSERT INTO tbl_Consulta_Trabajo VALUES(25);
+INSERT INTO tbl_Consulta_Trabajo VALUES(19);
+INSERT INTO tbl_Consulta_Trabajo VALUES(8);
+INSERT INTO tbl_Consulta_Trabajo VALUES(12);
+INSERT INTO tbl_Consulta_Trabajo VALUES(19);
+INSERT INTO tbl_Consulta_Trabajo VALUES(19);
